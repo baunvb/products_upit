9f3ae187304c2a8fafb417097d93a79d03c08838
baunvb <baunvb@gmail.com>
2021-01-10 23:12:00 +0700

    init project

---
+++ src/product/bikerental/Booking.js
@@ -0,0 +1,14 @@
+import "./booking.css"
+function Booking(props) {
+    return (
+        <div class="vertical-container background" id="main">
+            <div class="vertical-body">
+                <div class="container">
+                    <h2 class="title">Comming Soon</h2>
+                    <p>Dịch vụ thuê xe máy</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Booking;
